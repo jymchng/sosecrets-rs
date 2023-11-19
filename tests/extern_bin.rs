5dc1561eb9ec1d3b934c503734ba7f02c4bc033a
Soon Siang <jymchng@hotmail.com>
2023-11-20 00:36:49 +0800

    both tests passed!

--- tests/extern_bin.rs
+++ tests/extern_bin.rs
@@ -1,15 +1,15 @@
-// use sosecrets_rs::prelude::*;
-// use typenum::{consts::{U5}};
-// mod common;
+use sosecrets_rs::prelude::*;
+use typenum::consts::U5;
+mod common;
 
-// #[test]
-// fn test_expose_secret() {
-//     use common::{self, UseSecret};
-//     let secret = "MySecret";
-//     let new_secret: Secret<&str, U5, _> = Secret::new(secret);
-//     let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
-//         let returned_value = UseSecret::new(*exposed_secret);
-//         (exposed_secret, returned_value)
-//     });
-//     assert_eq!(returned_value.inner, secret);
-// }
+#[test]
+fn test_expose_secret() {
+    use common::{self, UseSecret};
+    let secret = "MySecret";
+    let new_secret: Secret<&str, U5, _> = Secret::new(secret);
+    let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
+        let returned_value = UseSecret::new((*exposed_secret).to_owned());
+        (exposed_secret, returned_value)
+    });
+    assert_eq!(returned_value.inner, secret);
+}
