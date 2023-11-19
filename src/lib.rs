ac79ffda095cb6ba6910ef61c37a5e90b631af20
Soon Siang <jymchng@hotmail.com>
2023-11-19 22:27:59 +0800

    trying to add typenum

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![feature(generic_const_exprs)]
 
-mod secret;
 mod macros;
+mod secret;
 
 pub mod prelude {
-    pub use crate::secret::*;
     pub use crate::macros::*;
+    pub use crate::secret::*;
 }
--- src/secret.rs
+++ src/secret.rs
@@ -1,35 +1,59 @@
 #![feature(generic_arg_infer)]
 #![feature(generic_const_exprs)]
+use typenum::{
+    consts::{U0, U1},
+    type_operators::IsLess,
+    Add1, False, IsEqual, Sum, True, UInt, UTerm, Unsigned, B1,
+};
+
 use crate::generic_const_predicate;
 
-pub struct Secret<T, const MEC: usize, const EC: usize = 0>(T);
+pub type AddU1<A> = <A as core::ops::Add<U1>>::Output;
 
-pub struct ExposedSecret<'brand, T, const MEC: usize, const EC: usize = 0>(
+pub struct Secret<
     T,
-    ::core::marker::PhantomData<fn(&'brand ()) -> &'brand ()>,
+    MEC: Unsigned,
+    EC: core::ops::Add<typenum::U1> + typenum::IsLess<MEC> + Unsigned = U0,
+>(
+    T,
+    core::marker::PhantomData<MEC>,
+    core::marker::PhantomData<EC>,
 );
 
-impl<T, const MEC: usize, const EC: usize> Secret<T, MEC, EC> {
+pub struct ExposedSecret<'brand, T, MEC: Unsigned, EC: Unsigned>(
+    T,
+    ::core::marker::PhantomData<fn(&'brand ()) -> &'brand ()>,
+    ::core::marker::PhantomData<MEC>,
+    ::core::marker::PhantomData<EC>,
+);
+
+impl<T, MEC: Unsigned, EC: core::ops::Add<typenum::U1> + Unsigned + typenum::IsLess<MEC>>
+    Secret<T, MEC, EC>
+{
     #[inline(always)]
-    pub const fn new(value: T) -> Self {
-        Self(value)
+    pub fn new(value: T) -> Self {
+        Self(value, <_>::default(), <_>::default())
     }
 
     #[inline(always)]
     pub fn expose_secret<ReturnType>(
         self,
         scope: impl FnOnce(ExposedSecret<'_, T, MEC, EC>) -> (ExposedSecret<'_, T, MEC, EC>, ReturnType),
-    ) -> (Secret<T, MEC, { EC + 1 }>, ReturnType)
+    ) -> (Secret<T, MEC, AddU1<EC>>, ReturnType)
     where
-        generic_const_predicate!(MEC > EC):,
-        Secret<T, MEC, { EC + 1 }>:,
+        AddU1<EC>: core::ops::Add<typenum::U1> + Unsigned + typenum::IsLess<MEC>,
     {
-        let (witness, returned_value) = scope(ExposedSecret(self.0, <_>::default()));
+        let (witness, returned_value) = scope(ExposedSecret(
+            self.0,
+            <_>::default(),
+            <_>::default(),
+            <_>::default(),
+        ));
         (Secret::new(witness.0), returned_value)
     }
 }
 
-impl<T, const MEC: usize, const EC: usize> ::core::ops::Deref for ExposedSecret<'_, T, MEC, EC> {
+impl<T, MEC: Unsigned, EC: Unsigned> ::core::ops::Deref for ExposedSecret<'_, T, MEC, EC> {
     type Target = T;
 
     #[inline(always)]
@@ -41,32 +65,31 @@ impl<T, const MEC: usize, const EC: usize> ::core::ops::Deref for ExposedSecret<
 #[cfg(test)]
 mod tests {
 
+    use super::*;
+    use typenum::consts::{U1, U2, U3, U4, U5};
+    #[derive(Debug)]
+    struct UseSecret<T> {
+        inner: T,
+    }
+    impl<T> UseSecret<T> {
+        fn new(value: T) -> Self {
+            Self { inner: value }
+        }
+    }
+
     #[test]
     fn test_expose_secret() {
-        use super::*;
-        #[derive(Debug)]
-        struct UseSecret {
-            inner: String,
-        }
-        impl UseSecret {
-            fn new(value: impl AsRef<str>) -> Self {
-                Self {
-                    inner: value.as_ref().to_string(),
-                }
-            }
-        }
-
-        let new_secret: Secret<&str, 2, 0> = Secret::new("mySecret");
+        let new_secret: Secret<String, U3> = Secret::new("mySecret".to_string());
         let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
-            let returned_value = UseSecret::new(*exposed_secret);
+            let returned_value = UseSecret::new(*exposed_secret.to_string());
             (exposed_secret, returned_value)
         });
-        assert_eq!("mySecret", returned_value.inner);
+        assert_eq!("mySecret", &returned_value.inner);
         let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
-            let returned_value = UseSecret::new(*exposed_secret);
+            let returned_value = UseSecret::new(*exposed_secret.to_string());
             (exposed_secret, returned_value)
         });
-        assert_eq!("mySecret", returned_value.inner);
+        assert_eq!("mySecret", &returned_value.inner);
         // let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
         //     let returned_value = UseSecret::new(*exposed_secret);
         //     (exposed_secret, returned_value)
@@ -76,18 +99,7 @@ mod tests {
 
     #[test]
     fn test_expose_secret_2() {
-        use super::*;
-        #[derive(Debug)]
-        struct UseSecret {
-            inner: i32,
-        }
-        impl UseSecret {
-            fn new(value: i32) -> Self {
-                Self { inner: value }
-            }
-        }
-
-        let new_secret: Secret<_, 2> = Secret::new(69);
+        let new_secret: Secret<_, U2> = Secret::new(69);
         let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
             let returned_value = UseSecret::new(*exposed_secret);
             (exposed_secret, returned_value)
