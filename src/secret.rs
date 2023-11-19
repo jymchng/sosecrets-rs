c08f1830c038adc78d22a889b3a9c6cfd97f0994
Soon Siang <jymchng@hotmail.com>
2023-11-20 00:36:15 +0800

    both tests passed!

--- src/secret.rs
+++ src/secret.rs
@@ -85,25 +85,25 @@ mod tests {
         }
     }
 
-    // #[test]
-    // fn test_expose_secret() {
-    //     let new_secret: Secret<String, U3> = Secret::new("mySecret".to_string());
-    //     let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
-    //         let returned_value = UseSecret::new(*exposed_secret.to_string());
-    //         (exposed_secret, returned_value)
-    //     });
-    //     assert_eq!("mySecret", &returned_value.inner);
-    //     let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
-    //         let returned_value = UseSecret::new(*exposed_secret.to_string());
-    //         (exposed_secret, returned_value)
-    //     });
-    //     assert_eq!("mySecret", &returned_value.inner);
-    //     // let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
-    //     //     let returned_value = UseSecret::new(*exposed_secret);
-    //     //     (exposed_secret, returned_value)
-    //     // });
-    //     // assert_eq!("mySecret", returned_value.inner);
-    // }
+    #[test]
+    fn test_expose_secret() {
+        let new_secret: Secret<String, U3> = Secret::new("mySecret".to_string());
+        let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
+            let returned_value = UseSecret::new((*exposed_secret).to_string());
+            (exposed_secret, returned_value)
+        });
+        assert_eq!("mySecret", &returned_value.inner);
+        let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
+            let returned_value = UseSecret::new((*exposed_secret).to_string());
+            (exposed_secret, returned_value)
+        });
+        assert_eq!("mySecret", &returned_value.inner);
+        // let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
+        //     let returned_value = UseSecret::new(*exposed_secret);
+        //     (exposed_secret, returned_value)
+        // });
+        // assert_eq!("mySecret", returned_value.inner);
+    }
 
     #[test]
     fn test_expose_secret_2() {
