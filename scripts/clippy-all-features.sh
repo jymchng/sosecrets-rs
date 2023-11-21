e3ee484de503ed4c4a195a980fd6a5a3498deeb9
Soon Siang <jymchng@hotmail.com>
2023-11-22 01:16:06 +0800

    added scripts

---
+++ scripts/clippy-all-features.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Array of feature names
+features=("cloneable-secret")
+
+# Calculate the total number of features
+total_features=${#features[@]}
+
+# Function to generate combinations of features
+generate_combinations() {
+    local index=$1
+    local combination=$2
+
+    if [ $index -eq $total_features ]; then
+        # Run cargo clippy with the current combination of features
+        echo "Running: cargo clippy --features $combination"
+        cargo clippy --features "$combination"
+    else
+        # Include the current feature in the combination and recurse
+        generate_combinations "$((index + 1))" "$combination ${features[index]}"
+        # Exclude the current feature and recurse
+        generate_combinations "$((index + 1))" "$combination"
+    fi
+}
+
+# Start generating combinations from index 0
+generate_combinations 0 ""
+
+echo "All feature combinations checked with clippy successfully."
---
+++ scripts/tests-all-features.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Array of feature names
+features=("cloneable-secret")
+
+# Calculate the total number of features
+total_features=${#features[@]}
+
+# Function to generate combinations of features
+generate_combinations() {
+    local index=$1
+    local combination=$2
+
+    if [ $index -eq $total_features ]; then
+        # Run cargo test with the current combination of features
+        echo "Running: cargo test --features $combination"
+        cargo test --features "$combination"
+    else
+        # Include the current feature in the combination and recurse
+        generate_combinations "$((index + 1))" "$combination ${features[index]}"
+        # Exclude the current feature and recurse
+        generate_combinations "$((index + 1))" "$combination"
+    fi
+}
+
+# Start generating combinations from index 0
+generate_combinations 0 ""
+
+echo "All feature combinations tested successfully."
