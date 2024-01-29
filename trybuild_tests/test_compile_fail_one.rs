dcf6eb7e012636c501753a8074174b44e53c214d
root <root@vmi1175516.contaboserver.net>
2024-01-29 19:01:53 +0100

    34, improved the test_compile_fail_one test

--- trybuild_tests/test_compile_fail_one.rs
+++ trybuild_tests/test_compile_fail_one.rs
@@ -1,15 +1,37 @@
-use sosecrets_rs::prelude::*;
-use typenum::consts::U5;
-mod common;
+use sosecrets_rs::{prelude::*, traits::ExposeSecret};
+use typenum::consts::U2;
+#[cfg(feature = "zeroize")]
+use zeroize::Zeroize;
+
+#[cfg(feature = "zeroize")]
+impl Zeroize for AStruct {
+    fn zeroize(&mut self) {
+        self._inner.zeroize()
+    }
+}
+
+struct AStruct {
+    _inner: i32,
+}
+
+struct ReferenceWrapper {
+    _inner: Option<AStruct>,
+}
+
+fn make_return_secret(a_struct_: AStruct) -> Secret<AStruct, U2> {
+    Secret::new(a_struct_)
+}
 
 fn main() {
-    use common::UseSecret;
+    let a_struct = AStruct { _inner: 69 };
 
-    let secret = "MySecret".to_owned();
-    let new_secret: Secret<_, U5, _> = Secret::new(secret);
-    let (new_secret, returned_value) = new_secret.expose_secret(|exposed_secret| {
-        let returned_value = UseSecret::new((*exposed_secret).to_owned());
-        returned_value
+    let secret_astruct = make_return_secret(a_struct);
+
+    let mut ref_wrapper = ReferenceWrapper { _inner: None };
+
+    let (_, _) = secret_astruct.expose_secret(|exposed_secret| {
+        ref_wrapper._inner.insert(*exposed_secret);
     });
-    assert_eq!(returned_value.inner, "MySecret".to_owned());
+
+    assert_eq!(ref_wrapper._inner.take().unwrap()._inner, 69);
 }
